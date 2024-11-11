987fab6023f999baab02d5e41ce0d9da7ba4b1a9
mistwil777 <leroulierwilfried@gmail.com>
2024-11-11 18:57:20 +0100

    test attack

---
+++ tests/test_attack.py
@@ -0,0 +1,12 @@
+import unittest
+from character import Character
+
+def test_attack(self):
+    hero = Character("Hero", 100)
+    enemy = Character("Enemy", 100)
+    hero.attack(enemy)
+    self.assertEqual(enemy.health, 90)
+    
+    
+if __name__ == '__main__':
+    unittest.main()
